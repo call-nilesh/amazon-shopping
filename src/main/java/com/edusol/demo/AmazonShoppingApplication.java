a5a2c41fee387ab5aa728f0a8381b0a288e73aaa
Jambhulkar <nileshjambhulkar015@gmail.com>
2020-05-17 04:34:21 -0700

    First Commit

---
+++ src/main/java/com/edusol/demo/AmazonShoppingApplication.java
@@ -0,0 +1,27 @@
+package com.edusol.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class AmazonShoppingApplication {
+
+	
+	@LoadBalanced
+	@Bean
+	public RestTemplate getTemplate() {
+		return new RestTemplate();
+	}
+	
+	
+	public static void main(String[] args) {
+		SpringApplication.run(AmazonShoppingApplication.class, args);
+	}
+
+}
